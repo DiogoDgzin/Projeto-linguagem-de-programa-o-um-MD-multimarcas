f7b5c5c574814b8e0916789c19b04d51350a8178
Diogo Ewerton <diogoesn1212@gmail.com>
2025-11-30 19:40:49 -0300

    Update NotaFiscalService.java

--- untitled/src/NotaFiscalService.java
+++ untitled/src/NotaFiscalService.java
@@ -1,16 +1,17 @@
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class NotaFiscalService {
 
-  private final List<NotaFiscal> notas = new ArrayList<>();
+    private final List<NotaFiscal> notas = new ArrayList<>();
 
+    public NotaFiscal emitirNota(Venda venda) {
+        NotaFiscal nota = new NotaFiscal((long) (notas.size() + 1), venda);
+        notas.add(nota);
+        return nota;
+    }
 
-  public NotaFiscal emitirNota(Venda venda) {
-    NotaFiscal nota = new NotaFiscal((long) (notas.size()+1), venda);
-    notas.add(nota);
-    return nota;
-  }
-
-  public List<NotaFiscal> listarNotas() { return notas; }
+    public List<NotaFiscal> listarNotas() {
+        return notas;
+    }
 }
